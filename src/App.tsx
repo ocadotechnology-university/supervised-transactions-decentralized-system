9d0831621cc14e0fc596529793c646be3466483a
igijarosz <igijarosz@gmail.com>
2026-04-14 17:24:12 +0200

    Changed BrowserRouter to HashRouter to accommodate GitHub pages integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
