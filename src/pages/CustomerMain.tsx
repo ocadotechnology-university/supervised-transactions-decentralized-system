3026131301dc7426c060ce3ae7a706ee5a1e9c78
mmrychlik <279515@student.pwr.edu.pl>
2026-04-20 01:06:27 +0200

    feat: add CustomerRegistration with minimal functionality fix: adjust SupervisorRegister payload and local storage data refactor: unify cryptoutils naming

---
+++ src/pages/CustomerMain.tsx
@@ -0,0 +1,11 @@
+
+import "../styles.css";
+
+export default function CustomerMain() {
+
+    return (
+        <div className="screen">
+            <h1 className="title">wip</h1>
+        </div>
+    );
+}
