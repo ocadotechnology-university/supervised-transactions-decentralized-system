dbc86ce8723c113d18501b60bcffb169f69f5de7
mmrychlik <279515@student.pwr.edu.pl>
2026-04-25 17:48:17 +0200

    feat: remove Supervisor authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import SupervisorAuth from "./components/SupervisorAuth";
 import SupervisorMain from "./components/SupervisorMain";
-import ProtectedRoute from "./utils/ProtectedRoute";
 import RegisterTrader from "./components/SupervisorRegister";
 import SupervisorTraderQR from "./components/SupervisorTraderQR.tsx";
 import CustomerRegistration from "./components/CustomerRegistration.tsx";
@@ -20,12 +18,9 @@ export default function App() {
               </Route>
 
               <Route path="/supervisor">
-                  <Route path="login" element={<SupervisorAuth />} />
-                  <Route element={<ProtectedRoute />}>
-                      <Route index element={<SupervisorMain />} />
-                      <Route path="registerTrader" element={<RegisterTrader />} />
-                      <Route path="registerTrader/qr" element={<SupervisorTraderQR />} />
-                  </Route>
+                  <Route index element={<SupervisorMain />} />
+                  <Route path="registerTrader" element={<RegisterTrader />} />
+                  <Route path="registerTrader/qr" element={<SupervisorTraderQR />} />
               </Route>
           </Routes>
       </Router>
--- src/utils/ProtectedRoute.tsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute() {
-    const isAuth = sessionStorage.getItem("isSupervisor") === "true";
-
-    return isAuth ? <Outlet /> : <Navigate to="/supervisor/login" replace />;
-}
