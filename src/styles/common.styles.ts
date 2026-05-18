61e4b0ddd74871f1d546584f59ebd653f26c8a5a
mmrychlik <279515@student.pwr.edu.pl>
2026-05-18 19:29:28 +0200

    refactor: split styles to different files

--- src/styles/common.styles.ts
+++ src/styles/common.styles.ts
@@ -73,45 +73,4 @@ export const Paragraph = styled.p`
     margin-top: -10px;
     margin-bottom: 10px;
     text-align: center;
-`
-
-export const PointsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 60px;
-`;
-
-export const Circle = styled.div`
-    width: 180px;
-    height: 180px;
-    background-color: #b3b3b3;
-    border-radius: 50%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-bottom: 15px;
-`;
-
-export const PointsValue = styled.span`
-    font-size: 64px;
-    font-weight: bold;
-    color: white;
-`;
-
-export const PointsLabel = styled.h2`
-    font-size: 32px;
-    font-weight: bold;
-    color: #111;
-    margin: 0;
-    letter-spacing: 2px;
-`;
-
-export const PointsGrid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    gap: 20px;
-    width: 100%;
-    max-width: 320px;
-    margin-top: 40px;
 `;
