cf726c8464e32fe693cb33d41b16e41c60a80108
mmrychlik <279515@student.pwr.edu.pl>
2026-06-08 10:25:31 +0200

    feat: ranking is updated during verification under one key feat: stopped saving unnecessary transaction data during verification

--- src/utils/leaderboard.ts
+++ src/utils/leaderboard.ts
@@ -1,77 +1,13 @@
-import type { Transaction } from "./types";
+import type { LeaderboardEntry } from "./types";
 import {STORAGE_KEYS} from "./localStorageKeys.ts";
 
-function isValidTransaction(data: any): data is Transaction {
-    return(
-        data &&
-        typeof data.customerData === "string" &&
-        typeof data.points === "number" &&
-        typeof data.signature === "string"
-    )
-}
+export function getScore(): LeaderboardEntry[] {
+    const rawData = localStorage.getItem(STORAGE_KEYS.SUPERVISOR_RANKING);
+    const customerScores: Record<string, number> = rawData ? JSON.parse(rawData) : {};
 
-function getSortedScores(customerScores: Record<string, number>) {
-    const entries = Object.entries(customerScores);
-
-    // .sort method transforms dict into array 
-    // [0] - object key
-    // [1] - object value
-    // .sort compares second entry with first to sort
-    entries.sort((first, second) => second[1] - first[1]);
-
-    const sortedLeaderboard = entries.map(entry => {
-        return {
-            customerData: entry[0],
-            points: entry[1]
-        };
-    });
-
-    return sortedLeaderboard;
-}
-
-function getUsedTransactions(): Transaction[] {
-    const transactions: Transaction[] = [];
-
-    const allKeys = Object.keys(localStorage);
-
-    allKeys.forEach(key => {
-        if (key === STORAGE_KEYS.SUPERVISOR_TRADERS) {
-            return;
-        }
-
-        const rawValue = localStorage.getItem(key);
-
-        if (rawValue) {
-            try {
-                const parsedData = JSON.parse(rawValue);
-                if(isValidTransaction(parsedData)){
-                    transactions.push(parsedData);
-                }
-            } catch (error) {
-                console.error(`Error of parsing JSON key: ${key}`, error);
-            }
-        }
-    });
-
-    return transactions;
-}
-
-export function calculateScore(){
-    const usedTransactions = getUsedTransactions();
-    const customerScores: Record<string, number> = {};
-
-    for (const token of usedTransactions) {
-
-        if (!token.customerData) continue;
-
-        if(customerScores[token.customerData]) {
-            customerScores[token.customerData] += token.points;
-        }
-        else {
-            customerScores[token.customerData] = token.points;
-        }
-        
-    }
-
-    return getSortedScores(customerScores);
+    return Object.entries(customerScores).map(([customerData, points]) => ({
+            customerData,
+            points
+        }))
+        .sort((a, b) => b.points - a.points);
 }
