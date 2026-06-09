0b8d2fdc2e43e6d1f32b18eb214ab97dae0f1453
mmrychlik <279515@student.pwr.edu.pl>
2026-06-09 15:05:30 +0200

    style: deleted empty lines and added missing space

--- src/hooks/useSessionExpiration.ts
+++ src/hooks/useSessionExpiration.ts
@@ -2,8 +2,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { STORAGE_KEYS } from "../utils/localStorageKeys";
 
-
-const EXPIRATION_TIME_MS = 12* 60 * 60 * 1000; // 12 hours
+const EXPIRATION_TIME_MS = 12 * 60 * 60 * 1000; // 12 hours
 const CHECK_INTERVAL_MS = 5 * 60 * 1000; // 5 minutes
 
 export function useSessionExpiration() {
@@ -19,14 +18,12 @@ export function useSessionExpiration() {
 
                 if (timeElapsed > EXPIRATION_TIME_MS) {
                     localStorage.clear();
-
                     window.location.href = '/';
                 }
             }
         };
 
         checkExpiration();
-
         const timer = setInterval(checkExpiration, CHECK_INTERVAL_MS);
         return () => clearInterval(timer);
     }, [location.pathname]);
