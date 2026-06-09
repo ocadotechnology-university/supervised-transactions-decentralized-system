1ead3505c2975839cfabfad5c891cb08c39c2036
mmrychlik <279515@student.pwr.edu.pl>
2026-06-09 11:51:44 +0200

    feat: role descriptions

--- src/pages/SupervisorMain.tsx
+++ src/pages/SupervisorMain.tsx
@@ -10,7 +10,7 @@ export default function SupervisorMain() {
 
             <ButtonContainer>
                 <Button onClick={() => navigate("/supervisor/register")}>
-                    REGISTER A TRADER
+                    REGISTER TRADERS
                 </Button>
                 <Button onClick={() => navigate("/supervisor/verify")}>
                     VERIFY POINTS
